250faaee797d38e0e3f84fe8761020b97c03c368
lkzm <lkzmkz@gmail.com>
2021-10-02 15:59:11 +0300

    :)

--- pat/urls.py
+++ pat/urls.py
@@ -24,4 +24,4 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
